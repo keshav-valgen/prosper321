d58f02b1747d40977b625b7c6f17c9c08a600e47
keshav-valgen <keshav@valgen.com>
2017-11-21 12:14:29 +0530

    added new script

---
+++ R/fuzzy_match.R
@@ -0,0 +1,23 @@
+#' @import fuzzyjoin
+#' @import dplyr
+#' @export text_match
+
+
+text_match = function(){
+  a <- data.frame(name = c('Ace Co', 'Bayes', 'asd', 'Bcy', 'Baes', 'Bays'),
+                  price = c(10, 13, 2, 1, 15, 1))
+  b <- data.frame(name = c('Ace Co.', 'Bayes Inc.', 'asdf'),
+                  qty = c(9, 99, 10))
+
+
+  return(stringdist_join(a, b,
+                         by = "name",
+                         mode = "left",
+                         ignore_case = FALSE,
+                         method = "jw",
+                         max_dist = 99,
+                         distance_col = "dist") %>%
+           group_by(name.x) %>%
+           top_n(1, -dist))
+
+}
