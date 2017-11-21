c9d8e0e9cc4a4c63d91ec2283bd00ef043cfc216
keshav-valgen <keshav@valgen.com>
2017-11-21 17:21:45 +0530

    updated fuzzy match

--- R/fuzzy_match.R
+++ R/fuzzy_match.R
@@ -3,12 +3,12 @@
 #' @export text_match
 
 
-text_match = function(){
-  a <- data.frame(name = c('Ace Co', 'Bayes', 'asd', 'Bcy', 'Baes', 'Bays'),
-                  price = c(10, 13, 2, 1, 15, 1))
-  b <- data.frame(name = c('Ace Co.', 'Bayes Inc.', 'asdf'),
-                  qty = c(9, 99, 10))
-
+text_match = function(a,b){
+  # a <- data.frame(name = c('Ace Co', 'Bayes', 'asd', 'Bcy', 'Baes', 'Bays'),
+  #                 price = c(10, 13, 2, 1, 15, 1))
+  # b <- data.frame(name = c('Ace Co.', 'Bayes Inc.', 'asdf'),
+  #                 qty = c(9, 99, 10))
+  #
 
   return(stringdist_join(a, b,
                          by = "name",
