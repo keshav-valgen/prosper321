f4ea241ebef4304600e194fcb19d083142cbe59f
keshav-valgen <keshav@valgen.com>
2017-11-22 09:51:10 +0530

    updated documentation

--- R/fuzzy_match.R
+++ R/fuzzy_match.R
@@ -1,14 +1,12 @@
+#'Doing String matching
+#'
+#' This fucntion matches two strings and give the count that how many matched
 #' @import fuzzyjoin
 #' @import dplyr
 #' @export text_match
 
 
 text_match = function(a,b){
-  # a <- data.frame(name = c('Ace Co', 'Bayes', 'asd', 'Bcy', 'Baes', 'Bays'),
-  #                 price = c(10, 13, 2, 1, 15, 1))
-  # b <- data.frame(name = c('Ace Co.', 'Bayes Inc.', 'asdf'),
-  #                 qty = c(9, 99, 10))
-  #
 
   return(stringdist_join(a, b,
                          by = "name",
