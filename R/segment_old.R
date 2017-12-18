a3f518e0821dd22e2d44593bbf9963b751552c6d
keshav-valgen <keshav@valgen.com>
2017-12-18 13:18:26 +0530

    handling missing and zero values code are added

--- R/segment_old.R
+++ R/segment_old.R
@@ -15,14 +15,36 @@ segment <- function(access_token, instance_url, object, field, newname){
   session <- c(sessionID = access_token,instanceURL = instance_u, apiVersion = api)
 
   data1 <- rforcecom.bulkQuery(session, myquery, myobject)
-  data1 <- na.omit(data1)
+  #data1 <- na.omit(data1)
 
+  # Select Missing values , Add new field and filled with MISSING level
+  new_DF <- data1[is.na(data1[,2]),]
+  if(nrow(new_DF) > 0){
+    new_DF$dist = "MISSING"
+  }
+
+  # select Zero Values, Add new field and filled with ZERO VALUES level
+  new_DF1 <- data1[data1[,2] == 0 & !(is.na(data1[,2])),]
+  if(nrow(new_DF1) > 0){
+    new_DF1$dist = "ZERO VALUES"
+  }
   # Data Treatment starts Here
-  data2 <- subset(data1, select = c(2))
-  data2 <- slider(data2, 5)
-  data1 <- cbind(data1, data2) # Derived values are binded to the original data
-  data1 <- subset(data1, select = c("Id", "dist"))
-  colnames(data1) <- c("Id", newname)
+  data2 <- data1[data1[,2] != 0 & !(is.na(data1[,2])),]
+  data3 <- subset(data2, select = c(2))
+  data3 <- slider(data3, 5)
+  data3 <- cbind(data2, data3) # Derived values are binded to the original data
+  data3 <- data3[,-2] # Remove replicate fields
+
+  # Add missing values and zero values
+  if(nrow(new_DF) > 0){
+    data3 = rbind(data3, new_DF)
+  }
+
+  if(nrow(new_DF1) > 0){
+    data3 = rbind(data3, new_DF1)
+  }
+  data1 <- subset(data3, select = c("Id", "dist"))
+  colnames(data1) <- c("strId", "dist")
 
   updater(access_token, instance_url, myobject, data1)
 }
